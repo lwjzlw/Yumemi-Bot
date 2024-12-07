d86213b6a89d8b214703c29759a3689f0a8fbb3d
ltcRandomWalk <litianchi@pku.edu.cn>
2024-12-07 18:17:15 +0800

    update chat

--- src/plugins/chat/__init__.py
+++ src/plugins/chat/__init__.py
@@ -4,9 +4,16 @@ from nonebot.plugin import PluginMetadata
 from nonebot.adapters import Message
 from nonebot.params import CommandArg
 from openai import AzureOpenAI
+import openai
 import os
 from .sys_msg import system_message
-
+from anthropic import Anthropic
+from nonebot.adapters.onebot.v11 import (
+    GroupMessageEvent,
+    GROUP,
+    Message,
+    MessageSegment
+)
 from .config import Config
 
 __plugin_meta__ = PluginMetadata(
@@ -22,15 +29,45 @@ config = get_plugin_config(Config)
 chat_event = nonebot.on_command("chat", priority=10, block=True)
 
 @chat_event.handle()
-async def chat_handler(args: Message=CommandArg()):
+async def chat_handler(event: GroupMessageEvent, args: Message=CommandArg()):
     if content := args.extract_plain_text():
+        await chat_event.send("梦美正在思考中……")
         response = get_response(content)
-        await chat_event.finish(response)
+        msgs = [ MessageSegment.at(event.user_id), "\n" + response ]
+        node_msg = []
+        for msg in msgs:
+            node_msg.append(
+                MessageSegment.node_custom(
+                    user_id=os.getenv("QQ_NUMBER"),
+                    nickname=os.getenv("QQ_ID"),
+                    content=msg,
+                )
+            )
+        await chat_event.finish(node_msg)
     else:
         await chat_event.finish("你想和梦美聊些什么？请输入内容啦！")
 
 
-def get_response(prompt: str):
+def get_claude_response(prompt: str):
+    try:
+        client = Anthropic(
+            base_url='https://api.openai-proxy.org/anthropic',
+            api_key=os.getenv("CLAUDE_API_KEY"),
+        )
+        
+        message = client.messages.create(
+            max_tokens=1024,
+            system=system_message,
+            messages=[
+			    {"role": "user", "content": prompt}
+            ],
+            model="claude-3-5-haiku-20241022"
+        )
+        return message.content[0].text
+    except Exception as e:
+        return repr(e)
+
+def get_azure_response(prompt: str):
     try:
         azure_endpoint = os.getenv("OPENAI_ENDPOINT")
         api_key = os.getenv("OPENAI_API_KEY")
@@ -56,3 +93,6 @@ def get_response(prompt: str):
         return response.choices[0].message.content
     except Exception as e:
         return repr(e)
+
+def get_response(prompt: str):
+    return get_claude_response(prompt)
